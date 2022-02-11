32f83a59031e2de55db46188a6e7d0d0da87905f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-12 00:03:28 +0600

    appy js in header section buy now button to change html content

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//appy js in header section buy now button to change html content
+document.getElementById('header-buy-now-btn').addEventListener('click', function(){
+    const headerBuyNowBtn = document.getElementById('header-title');
+    headerBuyNowBtn.innerText = 'I am Penguin';
+});
